77fd4f34a80d09ba83573a2bfab1415d3ae87843
Caroline Ahler <carolineahler@users.noreply.github.com>
2025-11-27 11:21:17 +0100

    check_arg function

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -20,11 +20,13 @@ animal_sounds <- function(animal, sound) {
 
 
 check_arg <- function(arg, n = 1) {
-  if (!rlang::is_character(arg, n = 1)) {
-    cli::cli_abort(c("{.var arg} must be a single string",
-                     "i" = "It was {.type {arg}} of length {length(arg)} instead."))
+  if (!rlang::is_character(arg, n = n)) {
+    cli::cli_abort(c("{.var {rlang::caller_arg(arg)}} must be a character vector of length {n}",
+                     "i" = "It was {.type {arg}} of length {length(arg)} instead."),
+                   call = rlang::caller_env())
   }
 
 }
+
 
 
