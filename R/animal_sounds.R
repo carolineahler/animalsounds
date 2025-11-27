15fdd8bb53415606be0698784a2d65c09956c9d3
Caroline Ahler <carolineahler@users.noreply.github.com>
2025-11-27 14:41:48 +0100

    ss

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -9,21 +9,28 @@
 #'
 #' @examples animals_sounds("dog", "bark")
 #'
-animal_sounds <- function(animal, sound) {
+animal_sounds <- function(animal, sound = NULL) {
 
   check_arg(animal)
+
+  if (is.null(sound)) {
+    paste0("The ", animal, " makes no sounds.")
+  } else {
+
 
   check_arg(sound)
 
   paste0("The ", animal, " says ", sound, "!")
+  }
 }
 
 
 check_arg <- function(arg, n = 1) {
   if (!rlang::is_character(arg, n = n)) {
-    cli::cli_abort(c("{.var {rlang::caller_arg(arg)}} must be a character vector of length {n}",
+    cli::cli_abort(c("{.var {rlang::caller_arg(arg)}} must be a character vector of length {n}.",
                      "i" = "It was {.type {arg}} of length {length(arg)} instead."),
-                   call = rlang::caller_env())
+                   call = rlang::caller_env(),
+                   class = "error_wrong_length_or_not_string")
   }
 
 }
